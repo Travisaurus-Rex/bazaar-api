d4ff12814f2c593c420d6e4c7a805268383fa0c6
Travis Adams <brokenCD@live.com>
2025-09-30 15:23:47 +0700

    create dynamo config

---
+++ src/config/dynamo.js
@@ -0,0 +1,13 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+    region: process.env.AWS_REGION || 'us-east-',
+    endpoint: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000'
+})
+
+export const dbClient = DynamoDBDocumentClient.from(client, {
+    marshallOptions: {
+        removeUndefinedValues: true
+    }
+})
