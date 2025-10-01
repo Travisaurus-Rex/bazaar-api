e4d7b3294d971dcede239a16b5544eac94ff2d57
Travis Adams <brokenCD@live.com>
2025-10-01 12:20:23 +0700

    hash user pw

--- src/models/users.js
+++ src/models/users.js
@@ -1,14 +1,26 @@
 import { dbClient } from "../config/dynamo.js";
 import { PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+import bcrypt from "bcryptjs";
+
+const SALT_ROUNDS = 10;
 
 export const createUser = async ({name, userId, email, password}) => {
-    const user = { name, userId, email, password, createdAt: new Date().toISOString()};
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = { 
+        name, 
+        userId, 
+        email, 
+        password: hashedPassword, 
+        createdAt: new Date().toISOString()
+    };
+
     try {
         await dbClient.send(new PutCommand({
             TableName: 'Users',
             Item: user
         }));
-        return user;
+
+        return { ...user, password: undefined };
     } catch (err) {
         console.error(err);
         throw err;
