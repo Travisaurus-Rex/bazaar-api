ddbe70be7580b0ccdfbecbd9b53dfddaae53eca0
Travis Adams <brokenCD@live.com>
2025-10-19 11:31:56 +0700

    add app test

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,12 +1,11 @@
 /* eslint-env jest */
 
-//import request from 'supertest';
+import request from 'supertest';
 import app from '../app';
 
 describe('GET /', () => {
     it('Should return 200 success', async () => {
-        const res = await app.get('/');
-        console.log(res);
+        const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe('success!');
     })
