8e88ac3e8c38de3e8e4f79f0719ca43ba9d0f77d
Travis Adams <brokenCD@live.com>
2026-01-13 16:10:13 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'fatal', 'log'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Bazaar API')
+    .setDescription('Marketplace API with auth, products, cart, orders')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
